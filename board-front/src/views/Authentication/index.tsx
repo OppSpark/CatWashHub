60bba551dfcd623f5cd7331a701ec2e474dc3057
OppSpark <picks5@naver.com>
2024-10-06 20:07:26 +0900

    login page 배경화면 및 구조화

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
 //컴포넌트 : 인증 화면 컴포넌트
 export default function Authentication() {
+  //상태 : 화면 상태
+  const [view, setView] = useState<"sign-in" | "sign-up">("sign-in");
+
+  //컴포넌트 : sign in card 컴포넌트
+  const SignInCard = () => {
+    return <div className="auth-card"></div>;
+  };
+
+  //컴포넌트 : sign iup card 컴포넌트
+  const SignUpCard = () => {
+    return <div className="auth-card"></div>;
+  };
 
   //렌더 : 컴포넌트 렌더링
   return (
-    <div>
-      인증 화면
+    <div id="auth-wrapper">
+      <div className="auth-container">
+        <div className="auth-jumbotron-box">
+          <div className="auth-jumbotron-contents">
+            <div className="auth-logo-icon"></div>
+            <div className="auth-jumbotron-text-box">
+              <div className="auth-jumbotron-text">{"catWashHub"}</div>
+              <div className="auth-jumbotron-text">
+                {"모두의 세차공간"}
+              </div>
+            </div>
+          </div>
+        </div>
+        {view === "sign-in" && <SignInCard />}
+        {view === "sign-up" && <SignUpCard />}
+      </div>
     </div>
-  )
+  );
 }
