a78e4a6ad2499c96b3f10e6a51f6c3871e0bee45
OppSpark <picks5@naver.com>
2024-10-28 19:48:27 +0900

    프론트엔드 로그인 api 작성

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,2 +1,16 @@
-export const tmp = '';
-//임시 생성
+import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
+
+const DOMAIN = 'http://localhost: 4000' ;
+
+const API_DOMAIN = `${DOMAIN}/api/v1` ;
+
+const SIGN_IN_URL = () =>  `${API_DOMAIN}/auth/sign-in`;
+const SIGN_UP_URL = () =>  `${API_DOMAIN}/auth/sign-up`;
+
+export const signInRequest = (requestBody: SignInRequestDto) => {
+
+}
+
+export const signUpRequest = (requestBody: SignUpRequestDto) => {
+
+}
